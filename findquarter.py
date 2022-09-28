a516d992c2738048bbfac9c514df554f15acd8de
QWuizt <boringfine@gmail.com>
2022-09-28 22:49:16 +0300

    определение четверти

---
+++ findquarter.py
@@ -0,0 +1,12 @@
+x = int(input("Введите X: "))
+y = int(input("Введите Y: "))
+if x > 0 and y > 0:
+    print("1 четверть")
+elif x < 0 and y > 0:
+    print("2 четверть")
+elif x < 0 and y < 0:
+    print(" 3 четверть")
+elif x > 0 and y < 0:
+    print("4 четверть")
+else:
+    print("X and Y != 0")
