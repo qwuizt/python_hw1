8c0c9212c3c466ac958f845b2179e54135e12ba0
QWuizt <boringfine@gmail.com>
2022-09-28 23:07:34 +0300

    диапозон координат в четвертях

---
+++ quarterRange.py
@@ -0,0 +1,11 @@
+quarter = int(input("Введите номер четверти (1,2,3,4): "))
+if quarter == 1:
+    print("X:(0;+∞) Y:(0;+∞)")
+elif quarter == 2:
+    print("X:(-∞;0) Y:(0;+∞)")
+elif quarter == 3:
+    print("X:(-∞;0) Y:(-∞;0)")
+elif quarter == 4:
+    print("X:(0;+∞) Y:(-∞:0)")
+else:
+    print("Введите номер четверти правильно")
